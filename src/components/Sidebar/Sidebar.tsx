4c034720116740152de868434364ac366db4d464
Andrey Lavrjonov <aalavrenov@phoenixit.ru>
2024-01-02 10:13:17 +0300

    fix: adds class active

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { PostsType, posts } from '../../consts/posts';
 import styles from './Sidebar.module.css';
 
@@ -7,21 +7,24 @@ type PropsType = {
 };
 
 const Sidebar = ({ setCurrentPosts }: PropsType) => {
+  const [activeCategory, setActiveCategory] = useState<string>('all');
+
   const categoryClick = (data: string) => {
     const temp = posts.filter((el) => el.category.includes(data));
     setCurrentPosts(temp);
+    setActiveCategory(data);
   };
 
   return (
     <aside className={styles.sidebar}>
       <ul className={styles.list}>
-        <li className={`${styles.item} ${styles.item_home}`} onClick={() => categoryClick('all')}>
+        <li className={activeCategory === 'all' ? `${styles.item} ${styles.item_home} ${styles.item_active}` : `${styles.item} ${styles.item_home}`} onClick={() => categoryClick('all')}>
           <span>Все посты</span>
         </li>
-        <li className={`${styles.item} ${styles.item_camp}`} onClick={() => categoryClick('big')}>
+        <li className={activeCategory === 'big' ? `${styles.item} ${styles.item_camp} ${styles.item_active}` : `${styles.item} ${styles.item_camp}`} onClick={() => categoryClick('big')}>
           <span>Большие походы</span>
         </li>
-        <li className={`${styles.item} ${styles.item_deer}`} onClick={() => categoryClick('nizh')}>
+        <li className={activeCategory === 'nizh' ? `${styles.item} ${styles.item_deer} ${styles.item_active}` : `${styles.item} ${styles.item_deer}`} onClick={() => categoryClick('nizh')}>
           <span>Нижегородская область</span>
         </li>
       </ul>
