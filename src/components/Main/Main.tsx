78c8194f353a85758b20e12db86cb3d00e5e33ad
Andrey Lavrjonov <aalavrenov@phoenixit.ru>
2026-01-10 16:35:55 +0300

    feat: corrects the display of photo hints

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import { Dispatch } from 'react';
+import { Dispatch, useEffect, useRef, useState } from 'react';
 import { PhotosType, PostsType } from '../../consts/posts';
 import styles from './Main.module.css';
 import Post from './Post/Post';
@@ -10,37 +10,50 @@ type PropsType = {
 };
 
 const Main = ({ currentPosts, setCurrentPhotos, setSlider }: PropsType) => {
-  const length = currentPosts.length;
+  const [gridHeight, setGridHeight] = useState('0px');
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (!listRef.current) return;
+    const observer = new ResizeObserver(entries => {
+      setGridHeight(`${entries[0].contentRect.height}px`);
+    });
+    observer.observe(listRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  const getSize = (length: number, index: number, size?: string) => {
+    switch (size) {
+      case 'big':
+        return {width: 2, height: 2};
+      case 'tall':
+        return {width: 1, height: 2};
+      case 'long':
+        return {width: 2, height: 1};
+      case 'small':
+        return {width: 1, height: 1};
+
+      default:
+        return {
+          width: (length - index < 6) ? 1 : Math.round(Math.random() + 1),
+          height: Math.round(Math.random() + 1)
+        };
+    }
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.list__wrapper}>
-        <ul className={styles.list} style={{gridTemplateRows: `repeat(${length}, 1fr)`}}>
+        <ul className={styles.list} ref={listRef} style={{height: gridHeight}}>
           {currentPosts.map((el, index) => {
-            const size = () => {
-              switch (el.size) {
-                case 'big':
-                  return {width: 2, height: 2};
-                case 'tall':
-                  return {width: 1, height: 2};
-                case 'long':
-                  return {width: 2, height: 1};
-                case 'small':
-                  return {width: 1, height: 1};
-                      
-                default:
-                  return {
-                    width: (length - index < 6) ? 1 : Math.round(Math.random() + 1),
-                    height: Math.round(Math.random() + 1)
-                  };
-              }
-            }
+            const {height, width} = getSize(currentPosts.length, index, el.size);
 
             return (
               <Post
                 key={el.title}
                 data={el.data}
-                gridColumn={size().width}
-                gridRow={size().height}
+                gridColumn={width}
+                gridRow={height}
                 photos={el.photos}
                 setCurrentPhotos={setCurrentPhotos}
                 setSlider={setSlider}
