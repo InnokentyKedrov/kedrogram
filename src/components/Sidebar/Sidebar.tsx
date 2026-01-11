98d5c1dd450136e3880493cc6be664f415445874
Andrey <kedr_club@mail.ru>
2026-01-11 10:15:55 +0300

    feat: adds new post muzeum1

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,7 +6,6 @@ type PropsType = {
   activeCategory: string;
   setActiveCategory: Dispatch<string>;
   setCurrentPosts: Dispatch<PostsType[]>;
-
 };
 
 const Sidebar = ({ activeCategory, setActiveCategory, setCurrentPosts }: PropsType) => {
@@ -94,6 +93,26 @@ const Sidebar = ({ activeCategory, setActiveCategory, setCurrentPosts }: PropsTy
             />
           </svg>
           <span>Москва</span>
+        </li>
+        <li
+          className={
+            activeCategory === 'novg' ? `${styles.item} ${styles.item_active}` : styles.item
+          }
+          onClick={() => categoryClick('novg')}
+        >
+          <svg
+            className={styles.item__image}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 56 65"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="5"
+          >
+            <path d="M40 62H5.8c-1.3 0-5.9-7.1-5.9-8s1-1.4 1-1.4l50-9.4c1.3 0 2 0.9 2 2.4C53 47 48.3 62 40 62z" />
+            <path d="M23 42.2L4.7 47.1c0 0 15.3-13 10.6-43.6" />
+            <path d="M22.4 42.4L46 36.5c0 0-4.7-29.4-33-36.5C13 0 28.3 17.7 22.4 42.4z" />
+          </svg>
+          <span>Великий Новгород</span>
         </li>
       </ul>
     </aside>
